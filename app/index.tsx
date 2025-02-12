8486582d94e8d0ca21ea0fb4758bea03c20d8363
Simrose18 <simrosekaurc18@gmail.com>
2025-02-12 16:39:18 -0500

    pomodoro done

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,73 @@
-import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { TimerCountDownDisplay } from "../components/TimerCountDownDisplay";
+import { TimerModeDisplay, TimerModes } from "../components/TimerModeDisplay";
+import { TimerToggleButton } from "../components/TimerToggleButton";
+import React from "react";
 
-export default function Index() {
+const FOCUS_TIME_MINUTES = 25 * 60 * 1000;
+const BREAK_TIME_MINUTES = 5.0 * 60 * 1000;
+
+export default function App() {
+  const [timerCount, setTimerCount] = useState<number>(FOCUS_TIME_MINUTES);
+  const [intervalId, setIntervalId] = useState<NodeJS.Timer | null>(null);
+  const [timerMode, setTimerMode] = useState<TimerModes>("Focus");
+  const [isTimerRunning, setIsTimerRunning] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (timerCount === 0) {
+      if (timerMode === "Focus") {
+        setTimerMode("Break");
+        setTimerCount(BREAK_TIME_MINUTES);
+      } else {
+        setTimerMode("Focus");
+        setTimerCount(FOCUS_TIME_MINUTES);
+      }
+      stopCountDown();
+    }
+  }, [timerCount]);
+
+  const startCountDown = () => {
+    setIsTimerRunning(true);
+    const id = setInterval(() => setTimerCount((prev) => prev - 1000), 1000);
+    setIntervalId(id);
+  };
+
+  const stopCountDown = () => {
+    setIsTimerRunning(false);
+    if (intervalId) {
+      clearInterval(intervalId);
+    }
+    setIntervalId(null);
+  };
+
   return (
     <View
       style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        ...styles.container,
+        ...{ backgroundColor: timerMode === "Break" ? "#2a9d8f" : "#d95550" },
       }}
     >
-      <Text>Edit app/index.tsx to edit this .</Text>
+      <StatusBar style="auto" />
+      <TimerModeDisplay timerMode={timerMode} />
+
+      <TimerToggleButton
+        startCountDownHandler={startCountDown}
+        isTimerRunning={isTimerRunning}
+        setIsTimerRunning={setIsTimerRunning}
+        stopCountDownHandler={stopCountDown}
+      />
+      <TimerCountDownDisplay countDownDate={new Date(timerCount)} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#d95550",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
