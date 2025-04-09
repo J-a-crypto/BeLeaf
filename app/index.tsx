f723674bb84b2e8b4895956afc79b8a8453db428
Simrose18 <simrosekaurc18@gmail.com>
2025-04-09 16:23:17 -0400

    changed

--- app/index.tsx
+++ app/index.tsx
@@ -26,7 +26,8 @@ export default function App() {
       }
       stopCountDown();
     }
-  }, [timerCount]);
+  }, 
+  [timerCount]);
 
   const startCountDown = () => {
     setIsTimerRunning(true);
